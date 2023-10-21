20327ce9a27bb9afec3976ccab95ce0d0d0ab043
antonyMeow <flewdash777@gmail.com>
2023-10-21 14:35:32 +0300

    birth date 3

--- Frontend/js/registration.js
+++ Frontend/js/registration.js
@@ -13,7 +13,7 @@ regForm.addEventListener('submit', (e) => {
 
   const username = inpUsername.value
   const surname = inpSurname.value
-  const birthDate = inpBirthDate.value.split('.').reverse().join(".")
+  const birthDate = inpBirthDate.value
   const phoneNumber = inpPhoneNumber.value
   const password = inpPassword.value
   const confirmPassword = inpConfirmPassword.value
@@ -70,7 +70,7 @@ regForm.addEventListener('submit', (e) => {
   const data = {
     name: username, 
     surname: surname, 
-    birthDate: birthDate,
+    birthDate: birthDate.split('.').reverse().join("-"),
     phoneNumber: phoneNumber, 
     password: password
   }
