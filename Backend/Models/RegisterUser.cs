58e3610161a821d572a3a4ca974bf18e161da406
ValeroK <kondratenko449@gmail.com>
2023-10-21 14:43:25 +0300

    Дата

--- Backend/Models/RegisterUser.cs
+++ Backend/Models/RegisterUser.cs
@@ -4,7 +4,7 @@
     {
         public string name { get; set; }
         public string surname { get; set; }
-        public DateTime dataBirthday { get; set; }
+        public DateTime birthDate { get; set; }
         public string phonenumber { get; set; }
         public string password { get; set; }
         public bool? admin { get; set; }
